207c16be873b9b46e791dcfc4b3f8230d140ea53
shaddowlinkk <nathan.vanbrunt@gmail.com>
2023-10-19 22:13:45 -0400

    more testing with escape codes

--- src/tui.c
+++ src/tui.c
@@ -28,6 +28,42 @@ int getconchar( KEY_EVENT_RECORD *krec )
 return 0; //future ????
 }
 
+void updateDebug(int curline){
+    printf("\x1b[?25l");
+    printf("\x1b[s\x1b[1;1H\x1b(0l\x1b(B");
+    for (int i = 1; i < 5; i++)printf("\x1b(0q\x1b(B");
+    printf("\x1b(0w\x1b(B\x1b");
+    for (int i = 6; i < 119; i++)printf("\x1b(0q\x1b(B");
+    printf("\x1b(0k\x1b(B\x1b[1E");
+
+    for (int j=1;j<35;j++) {
+       /* char *hl=( j==curline && j!=18)? "30;97":"27";
+        hl=( j!=curline && j!=18)? "30;97":"27";*/
+        printf("\x1b(0x\x1b(B"
+               "%d"//line numbers
+               "\x1b[6G\x1b(0x\x1b(B"
+               "\x1b[%sm"
+               "%s"
+               "\x1b[27m"//code
+               "\x1b(0\x1b[120Gx\x1b(B\x1b[1E",curline+j-1,"","test");
+    }
+
+    printf("\x1b(0t\x1b(B");
+
+    for (int i = 1; i < 5; i++)printf("\x1b(0q\x1b(B");
+    printf("\x1b(0v\x1b(B\x1b");
+    for (int i = 6; i < 119; i++)printf("\x1b(0q\x1b(B");
+    printf("\x1b(0u\x1b(B");
+
+    for (int j=35;j<51;j++) {//stack ans register area
+        printf("\x1b(0x\x1b[120Gx\x1b(B\x1b[1E");
+    }
+
+    printf("\x1b(0m\x1b(B");
+    for (int i = 1; i < 119; i++)printf("\x1b(0q\x1b(B");
+    printf("\x1b(0j\x1b(B\x1b[u");
+    printf("\x1b[?25h");
+}
 int test(){
     // Set output mode to handle virtual terminal sequences
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -35,16 +71,27 @@ int test(){
     GetConsoleMode(hOut, &dwMode);
     dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
     dwMode |=DISABLE_NEWLINE_AUTO_RETURN;
+    COORD size;
+    SMALL_RECT box;
+    box.Bottom=49;
+    box.Right=119;
+    box.Top=0;
+    box.Left=0;
+    size.X=120;
+    size.Y=50;
+    SetConsoleScreenBufferSize(hOut,size);
+    SetConsoleWindowInfo(hOut,1,&box);
     SetConsoleMode(hOut, dwMode);
     printf("\x1b[2 q");
-    printf("\x1B[34mtest\r\n");
-    wprintf(L"\x1B[32mtest\x1B[39m\r\n");
     CONSOLE_SCREEN_BUFFER_INFO info;
+    GetConsoleScreenBufferInfo(hOut, &info);
     KEY_EVENT_RECORD key;
+    int line=0;
+    updateDebug(line);
     while(1){
-
+        SetConsoleWindowInfo(hOut,1,&box);
         GetConsoleScreenBufferInfo(hOut, &info);
-        printf("\x1b[s\x1b[30;111H\x1b[31mX:%hi,Y:%hi\x1b[u",info.dwCursorPosition.X,info.dwCursorPosition.Y);
+        printf("\x1b[s\x1b[49;108H\x1b[31mX:%hi,Y:%hi\x1b[u\x1b[27m",info.dwCursorPosition.X+1,info.dwCursorPosition.Y+1);
         getconchar( &key);
         switch (key.wVirtualKeyCode) {
             case VK_UP:{
@@ -65,15 +112,20 @@ int test(){
             }
             case VK_F1:{
                 GetConsoleScreenBufferInfo(hOut, &info);
-                printf("\x1b[36mX:%hi,Y:%hi",info.dwCursorPosition.X,info.dwCursorPosition.Y);
+                printf("\x1b[36mX:%hi,Y:%hi\x1b[27m",info.dwCursorPosition.X,info.dwCursorPosition.Y);
                 break;
             }
             case VK_F2:{
                 wprintf(L"\x1B[2J\x1B[3J");//<<<clears the screen
                 break;
             }
+            case VK_F3:{
+                line++;
+                updateDebug(line);
+                break;
+            }
             case VK_RETURN:{
-                wprintf(L"\x1b[32mhello");
+                wprintf(L"\x1b[32mhello\x1b[27m");
                 break;
             }
         }
